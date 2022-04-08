d684bcaa01eef966af0faaea49bf03008545b267
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-08 15:16:11 -0300

    Filtro por categoria

--- 01-react/Roadmap-formacao.md
+++ 01-react/Roadmap-formacao.md
@@ -2,4 +2,4 @@
     [x] Fundamentode React - Escrevendo com Typescript
     [x] Curso React - Abstraindo seu Css com Styled Components
     [x] Curso React - Lidando com arquivos estáticos
-    [1] Curso React Router - Navegação em uma SPA 
+    [5] Curso React Router - Navegação em uma SPA 
--- 01-react/alura-petshop/src/paginas/Post.jsx
+++ 01-react/alura-petshop/src/paginas/Post.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { busca } from '../api/api';
 import '../assets/css/post.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 export default function Post() {
+    let history = useHistory ();
     const {id} = useParams();
     const [post, setPost] = useState({});
 
     useEffect(() => {
         busca(`/posts/${id}`, setPost)
+        .catch(()=> {
+            history.push('/404');
+        })
     }, [id])
   return (
     <main className="container flex flex--centro">
