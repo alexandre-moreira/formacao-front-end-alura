9e12f14e6831e1a77ce1bd72b1bd03c99810b1c1
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-08 16:34:16 -0300

    Criando subcategoria

--- 01-react/alura-petshop/src/components/ListaCategorias.jsx
+++ 01-react/alura-petshop/src/components/ListaCategorias.jsx
@@ -1,27 +1,29 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { busca } from '../api/api'
-import '../assets/css/blog.css';
+import '../assets/css/blog.css'
 
-export default function ListaCategorias() {
-    const [categorias, setCategorias] = useState([]);
+const ListaCategorias = () => { 
 
-    useEffect(()=> {
-        busca(`/categorias`, setCategorias)
-    },[])
+  const[categorias, setCategorias] = useState([])
 
-  return (
+  useEffect(() => {
+   busca(`/categorias`, setCategorias)
+  }, [])
+
+  return(
     <ul className="lista-categorias container flex">
-        {
-            categorias.map((categoria)=>(
-                <Link to={`/categoria/${categoria.id}`}>
-                    <li className={`lista-categorias__categoria lista-categorias__categoria--${categoria.id}`}>
-                        {categoria.nome}
-
-                    </li>
-                </Link>
-            ))
-        }
+      {
+        categorias.map((categoria) => (
+          <Link to={`/categoria/${categoria.id}`}>
+            <li className={`lista-categorias__categoria lista-categorias__categoria--${categoria.id}`}>
+              {categoria.nome}
+            </li>
+          </Link>
+        ))
+      }
     </ul>
   )
 }
+
+export default ListaCategorias
--- 01-react/alura-petshop/src/paginas/Categoria.jsx
+++ 01-react/alura-petshop/src/paginas/Categoria.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
-import '../assets/css/blog.css';
-import ListaCategorias from '../components/ListaCategorias';
-import ListaPost from '../components/ListaPost';
-import { Route, useParams, useRouteMatch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import ListaCategorias from '../components/ListaCategorias'
+import ListaPost from '../components/ListaPost'
+import '../assets/css/blog.css'
+import { Route, useParams, useRouteMatch, Link, Switch } from 'react-router-dom'
+import { busca } from '../api/api'
+import SubCategoria from '../paginas/SubCategoria'
 
-export default function Categoria() {
-    const {id} = useParams();
-    const {path} = useRouteMatch();
+
+const Categoria = () => {
+  const { id } = useParams()
+  const { url, path } = useRouteMatch()
+  const [subcategorias, setSubCategorias] = useState([])
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subcategorias)
+    })
+  }, [id])
   return (
     <>
-        <div className="container">
-            <h2 className="titulo-pagina">Pet Notícias</h2>
-        </div>
-        <ListaCategorias />
+      <div className="container">
+        <h2 className="titulo-pagina">Pet Notícias</h2>
+      </div>
+
+      <ListaCategorias />
+      <ul className="lista-categorias container flex">
+        {
+          subcategorias.map((subcategoria) => (
+            <li
+              className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+              key={subcategoria}
+            >
+              <Link to={`${url}/${subcategoria}`}>
+                {subcategoria}
+              </Link>
+            </li>
+          ))
+
+        }
+      </ul>
+      <Switch>
         <Route exact path={`${path}/`}>
-            <ListaPost url={`/posts?categoria=${id}`}/>
+          <ListaPost url={`/posts?categoria=${id}`} />
         </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
     </>
   )
 }
+export default Categoria
