f26ea80888baa085793afb8b3855ae8861d177c4
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-11 16:17:41 -0300

    Criando componente  tela de inicio

--- 01-react/aluroni-router-dom/src/pages/Cardapio/index.tsx
+++ 01-react/aluroni-router-dom/src/pages/Cardapio/index.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import Filtros from './Filtros';
 import Itens from './Itens';
 import Ordenador from './Ordenador';
+import stylesTema from 'styles/Temas.module.scss';
+
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
@@ -13,7 +15,7 @@ export default function Cardapio() {
   return (
     <main>
       <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <h3 className={stylesTema.titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro} />
