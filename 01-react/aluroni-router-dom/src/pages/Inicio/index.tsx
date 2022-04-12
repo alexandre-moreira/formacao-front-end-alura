83b9dc22c447d57d315b1a96ae60d2cad34d3662
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-12 12:50:54 -0300

    Criando a pagina Prato

--- 01-react/aluroni-router-dom/src/pages/Inicio/index.tsx
+++ 01-react/aluroni-router-dom/src/pages/Inicio/index.tsx
@@ -2,11 +2,15 @@ import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Temas.module.scss';
 import nosaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Inicio(){
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
-
+  const navigate = useNavigate();
+  function RedirecionarParaDetalhes(prato: typeof cardapio[0]){
+    navigate(`/prato/${prato.id}`, {state: {prato}, replace: true});
+  }
   return(
     <section>
       <h3 className={stylesTema.titulo}>
@@ -18,7 +22,10 @@ export default function Inicio(){
             <div className={styles.recomendado__imagem}>
               <img src={item.photo} alt={item.title} />
             </div>
-            <button className={styles.recomendado__botao}>
+            <button 
+              className={styles.recomendado__botao}
+              onClick={() => RedirecionarParaDetalhes(item)}
+            >
               Ver mais
             </button>
           </div>
