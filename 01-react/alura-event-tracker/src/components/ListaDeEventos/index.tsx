9df198768372d364f26700be310777c4a8459276
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-13 18:19:58 -0300

    Deletando evento

--- 01-react/alura-event-tracker/src/components/ListaDeEventos/index.tsx
+++ 01-react/alura-event-tracker/src/components/ListaDeEventos/index.tsx
@@ -7,15 +7,14 @@ import style from './ListaDeEventos.module.scss';
 
 const ListaDeEventos: React.FC<{ 
   aoAlterarStatus: (id: number) => void, 
-  aoDeletarEvento: (id: number) => void, 
-  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoAlterarStatus, aoFiltroAplicado }) => {
     const eventos = useRecoilValue(listaDeEventosState);
 
   return (<section>
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
     <div className={style.Scroll}>
       {eventos.map(evento => (
-        <Evento aoAlterarStatus={aoAlterarStatus} aoDeletarEvento={aoDeletarEvento} evento={evento} key={evento.id} />
+        <Evento aoAlterarStatus={aoAlterarStatus} evento={evento} key={evento.id} />
       ))}
     </div>
   </section>)
