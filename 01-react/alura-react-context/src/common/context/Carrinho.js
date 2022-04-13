519bd434bdabf796ec093f8379ba8d7b5f3c8186
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-13 12:54:11 -0300

    Contando a quantidade de produtos

--- 01-react/alura-react-context/src/common/context/Carrinho.js
+++ 01-react/alura-react-context/src/common/context/Carrinho.js
@@ -1,19 +1,32 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = "Carrinho";
 
 export const CarrinhoProvider = ({ children }) => {
   const [carrinho, setCarrinho] = useState([]);
+  const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
   return (
-    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        setCarrinho,
+        quantidadeProdutos,
+        setQuantidadeProdutos
+        }}
+      >
       {children}
     </CarrinhoContext.Provider>
   )
 };
 
 export const useCarrinhoContext = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+  const {
+    carrinho, 
+    setCarrinho,
+    quantidadeProdutos,
+    setQuantidadeProdutos
+  } = useContext(CarrinhoContext);
 
   function mudarQuantidade(id, quantidade) {
     return carrinho.map(itemDoCarrinho => {
@@ -30,10 +43,7 @@ export const useCarrinhoContext = () => {
         [...carrinhoAnterior, novoProduto]
       )
     }
-    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
-      if(itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
-      return itemDoCarrinho;
-    }))
+    setCarrinho(mudarQuantidade(novoProduto.id, 1));
   }
 
   function removerProduto(id) {
@@ -45,10 +55,16 @@ export const useCarrinhoContext = () => {
     setCarrinho(mudarQuantidade(id, -1));
   }
 
+  useEffect(() => {
+    const novaQuantidade = carrinho.reduce((contador, produto) => contador + produto.quantidade, 0);
+    setQuantidadeProdutos(novaQuantidade);
+  }, [carrinho, setQuantidadeProdutos]);
+
   return {
     carrinho,
     setCarrinho,
     adicionarProduto,
-    removerProduto
+    removerProduto,
+    quantidadeProdutos
   }
 }
