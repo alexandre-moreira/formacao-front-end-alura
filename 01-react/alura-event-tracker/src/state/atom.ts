d600cd1cd5d0876fe45f500dc07ed9405dc1b6a3
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-14 11:35:59 -0300

    Criando o filtro

--- 01-react/alura-event-tracker/src/state/atom.ts
+++ 01-react/alura-event-tracker/src/state/atom.ts
@@ -1,5 +1,6 @@
 import { atom } from 'recoil';
 import { IEvento } from '../interfaces/IEvento';
+import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
 
 export const listaDeEventosState = atom<IEvento[]>({
     key: 'listaDeEventosState',
@@ -18,3 +19,8 @@ export const listaDeEventosState = atom<IEvento[]>({
         "id": 1642342959,
       },]
 });
+
+export const filtroDeEventos = atom<IFiltroDeEventos>({
+  key: 'filtroDeEventos',
+  default: {}
+})
